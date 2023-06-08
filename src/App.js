a5c402004ab8decc2dfb880d96b1dd13505cf98e
vamsi <vamsivarada3008@gmail.com>
2023-06-08 22:37:02 +0530

    added taim remaining to the page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ function App() {
   const [input, setInput] = useState('');
   const [value, setValue] = useState('hello world');
   const [accuracy, setAccuracy] = useState(0);
+  const [keyCount, setKeyCount] = useState(0);
   const originalStringRef = useRef('');
   const originalStringArrayRef = useRef([]);
   const inputStringArrayRef = useRef([]);
+  const [timer, setTimer] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
 
+
+  
   const generateRandomString = useMemo(() => {
     const characters = 'asdfghjkl';
     const charactersLength = characters.length;
@@ -20,6 +25,7 @@ function App() {
       }
       return result;
     };
+    
   }, []);
 
   const generate = useCallback(() => {
@@ -32,11 +38,8 @@ function App() {
     return temp;
   }, [generateRandomString]);
 
-  useEffect(() => {
-    const randomString = generate();
-    originalStringRef.current = randomString;
-    setValue(randomString);
-  }, [generate]);
+ 
+
 
   const handleInput = useCallback((e) => {
     const inputValue = e.target.value;
@@ -67,6 +70,56 @@ function App() {
     setAccuracy(parseInt(accuracyValue));
   }, []);
 
+   
+
+
+  useEffect(() => {
+    const randomString = generate();
+    originalStringRef.current = randomString;
+    setValue(randomString);
+    
+  }, [generate]);
+  useEffect(() => {
+    
+    const handleKeyDown = (e) => {
+      // if e.key is a letter
+      if(e.keyCode >= 65 && e.keyCode <= 90) {
+      
+      setKeyCount((prevCount) => prevCount + 1);
+      
+      if(!timerOn){
+        setTimerOn(true);
+        setTimer(60);
+      }
+    }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+    
+      window.removeEventListener('keydown', handleKeyDown);
+      
+      
+    };
+  }, [timerOn]);
+
+  useEffect(() => {
+    if(timerOn && timer>0){
+      const intervalId=setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+      return () => {
+        clearInterval(intervalId);
+      };
+
+    }
+    else if( timerOn && timer===0 ){
+      setTimerOn(false);
+      setKeyCount(0);
+      alert(`Your typing speed is ${keyCount} keys per minute`);
+      window.location.reload(false);
+    }
+  }, [timer, timerOn, keyCount]);
+
   return (
     <div>
       <h1>Typing Keys</h1>
@@ -77,6 +130,8 @@ function App() {
       <div className='tryagain'>
         <button onClick={() => window.location.reload(false)}>Try Again</button>
       </div>
+      <div className='keycount'>{keyCount}</div>
+      <div className='timer'>{timer}</div>
     </div>
   );
 }
